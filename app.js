7bc06164f822d6ff3aa3e6ae6955629105c5161f
Ishan Bajaj <ishanb007@gmail.com>
2023-07-19 14:29:24 +0530

    Level 1 Authentication: comparing passw with db

---
+++ app.js
@@ -0,0 +1,62 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function (req,res){
+    res.render("home");
+});
+
+app.route("/register")
+
+.get(function (req,res){
+    res.render("register");
+})
+.post(function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password:req.body.password
+    });
+    newUser.save().then(()=>{
+        res.render("secrets");
+      }).catch((err)=>{
+        console.log(err);
+      });
+});
+
+app.route("/login")
+.get(function (req,res){
+    res.render("login");
+})
+.post(async function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.findOne({email: username}).exec().then((foundItem)=>{
+        if(foundItem.password === password){
+            res.render("secrets");
+        }
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
